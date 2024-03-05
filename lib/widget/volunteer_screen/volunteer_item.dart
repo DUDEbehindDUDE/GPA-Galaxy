accf1226dc004640f9e469989cc12129b995ab1a
DUDEbehindDUDE <jnbrooks457@gmail.com>
2024-03-04 22:12:21 -0500

    Prevent overflow on volunteer screen

--- lib/widget/volunteer_screen/volunteer_item.dart
+++ lib/widget/volunteer_screen/volunteer_item.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:gpa_galaxy/class/util.dart';
 import 'package:gpa_galaxy/generics/type_adapters/volunteer.dart';
 
-class VolunteerItem extends StatelessWidget {
+class VolunteerItem extends StatelessWidget { 
   final String profile;
   final List<Volunteer> items;
   final int year;
+
 
   const VolunteerItem({
     super.key,
@@ -14,7 +16,8 @@ class VolunteerItem extends StatelessWidget {
     required this.year,
   });
 
-  List<Widget> _renderItems() {
+  List<Widget> _renderItems(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
     var style = const TextStyle(fontSize: 18.0);
     var descStyle = const TextStyle(color: Colors.grey);
 
@@ -23,7 +26,7 @@ class VolunteerItem extends StatelessWidget {
       Row(
         children: [
           SizedBox(
-            width: 56,
+            width: 66,
             child: Text("Date", style: descStyle),
           ),
           Text("Name", style: descStyle),
@@ -38,13 +41,20 @@ class VolunteerItem extends StatelessWidget {
       items.add(Row(
         children: [
           SizedBox(
-            width: 56,
+            width: 66,
             child: Text(
               Util.renderDateMD(volunteerItem.date),
               style: style,
             ),
           ),
-          Text(volunteerItem.name, style: style),
+          ConstrainedBox(
+            constraints: BoxConstraints(maxWidth: screenWidth - 150),
+            child: Text(
+              volunteerItem.name,
+              style: style,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
           const Spacer(),
           Text(volunteerItem.hours.toString(), style: style),
         ],
@@ -69,7 +79,7 @@ class VolunteerItem extends StatelessWidget {
               fontSize: 26,
             ),
           ),
-          ..._renderItems(),
+          ..._renderItems(context),
           const Padding(padding: EdgeInsets.only(bottom: 24.0))
         ],
       ),
